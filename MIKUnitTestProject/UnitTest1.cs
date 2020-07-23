d26b1a6681542ae1ada1a60965a3f4372b3715f9
Alex Tikhinkiy <microniy@mail.ru>
2020-07-23 15:44:13 +0300

    Announced interface INode with methods Add, Remove

--- MIKUnitTestProject/UnitTest1.cs
+++ MIKUnitTestProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System.Collections.Generic;
+using MikTecnology;
 
 namespace MIKUnitTestProject
 {
@@ -9,24 +10,81 @@ namespace MIKUnitTestProject
         public void Setup()
         {
         }
-
-        [Test]
-        public void TestLinkNodeds()
-        {
-            class TestNode : INode
-        {
-            private IList<INode> _nodes;
-            public IList<INode> Nodes => _nodes;
-            public TestNode()
-            {
-
-            }
-            public void Add(INode nod)
-            {
-
-            }
+        class TestNode : BaseNode
+        {          
         }
-            Assert.Pass();
+        [Test(Description ="INode test method Add")]
+        public void TestINodeAddOne()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();           
+            Assert.AreEqual(n1.Nodes.Count, 0);
+            n1.Add(n2);
+            Assert.AreEqual(n1.Nodes.Count, 1);           
         }
+        [Test(Description = "INode test method Add")]
+        public void TestINodeAddTwo()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            Assert.AreEqual(n1.Nodes.Count, 0);
+            n1.Add(n2);
+            n1.Add(n2);
+            Assert.AreEqual(n1.Nodes.Count, 1);
+            TestNode n3 = new TestNode();
+            n1.Add(n3);
+            Assert.AreEqual(n1.Nodes.Count, 2);
+        }
+        [TestCase(100, Description = "INode test method many Add")]
+        public void TestINodeAddMany(int value)
+        {
+            TestNode n1 = new TestNode();       
+            
+            for(int i = 0; i < value; i++)
+            {
+                TestNode n2 = new TestNode();
+                Assert.AreEqual(n1.Nodes.Count, i);
+                n1.Add(n2);
+            }
+                     
+            Assert.AreEqual(n1.Nodes.Count, 100);
+        }
+        [Test(Description = "INode test Add oneself")]
+        public void TestINodeAddSelf()
+        {
+            TestNode n1 = new TestNode();          
+            Assert.AreEqual(n1.Nodes.Count, 0);
+            n1.Add(n1);
+            Assert.AreEqual(n1.Nodes.Count, 0);
+        }
+        [Test(Description = "INode test method Remove")]
+        public void TestINodeRemoveOne()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            Assert.AreEqual(n1.Nodes.Count, 0);
+            n1.Add(n2);               
+            Assert.AreEqual(n1.Nodes.Count, 1);
+            n1.Remove(n2);
+            Assert.AreEqual(n1.Nodes.Count, 0);
+        }
+        [Test(Description = "INode test method Remove")]
+        public void TestINodeRemoveTwo()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            Assert.AreEqual(n1.Nodes.Count, 0);
+            n1.Add(n2);
+            n1.Add(n3);
+            Assert.AreEqual(n1.Nodes.Count, 2);
+            n1.Remove(n2);
+            Assert.AreEqual(n1.Nodes.Count, 1);
+            n1.Remove(n2);
+            Assert.AreEqual(n1.Nodes.Count, 1);
+            n1.Remove(n3);
+            Assert.AreEqual(n1.Nodes.Count, 0);
+        }
+
     }
 }
---
+++ MikTecnology/Conventions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikTecnology
+{
+    public interface INode
+    {      
+        void Add(INode node);
+        void Remove(INode node);
+    }
+    public abstract class BaseNode : INode
+    {
+        private IList<INode> _nodes = new List<INode>();
+        public IList<INode> Nodes => _nodes;       
+        public virtual void Add(INode node)
+        {
+            if ((!this.Equals(node))&&(!_nodes.Contains(node)))
+                _nodes.Add(node);
+        }
+        public void Remove(INode node)
+        {
+            if (_nodes.Contains(node))
+                _nodes.Remove(node);
+        }
+    }
+}
