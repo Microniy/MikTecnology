ab77ef9a002f4cf9c4f51788e2aced357de146ae
Alex Tikhinkiy <microniy@mail.ru>
2020-07-24 13:23:16 +0300

    Announced interface IVersion with method AddVersion. Implementated get Old Next version

--- MIKUnitTestProject/UnitTest1.cs
+++ MIKUnitTestProject/UnitTest1.cs
@@ -11,7 +11,12 @@ namespace MIKUnitTestProject
         {
         }
         class TestNode : BaseNode
-        {          
+        {
+            public new IList<IVersion> Versions;
+            public TestNode()
+            {
+                this.Versions = base.Versions;
+            }
         }
         [Test(Description ="INode test method Add")]
         public void TestINodeAddOne()
@@ -85,6 +90,179 @@ namespace MIKUnitTestProject
             n1.Remove(n3);
             Assert.AreEqual(n1.Nodes.Count, 0);
         }
-
+        [Test(Description = "IVersion test method AddVersion")]
+        public void TestIVersionAddOne()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();          
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+        }
+        [Test(Description = "IVersion test method AddVersion")]
+        public void TestIVersionAddTwo()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n1.AddVersion(n3);
+            Assert.AreEqual(n1.Versions.Count, 2);
+        }
+        [Test(Description = "IVersion test across method AddVersion")]
+        public void TestIVersionAddTwoAcross()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n2);            
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n2.AddVersion(n3);
+            Assert.AreEqual(n1.Versions.Count, 2);
+        }
+        [Test(Description = "IVersion test across method AddVersion")]
+        public void TestIVersionAddAcrossOnlyBasedLink()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            TestNode n4 = new TestNode();
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n2);           
+            n2.AddVersion(n3);
+            n2.AddVersion(n4);
+            Assert.AreEqual(n2.Versions.Count, 0);
+        }
+        [Test(Description = "IVersion test across method AddVersion")]
+        public void TestIVersionAddTreeAcross()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            TestNode n4 = new TestNode();
+            TestNode n5 = new TestNode();
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n2.AddVersion(n3);
+            Assert.AreEqual(n1.Versions.Count, 2);
+            n2.AddVersion(n4);
+            Assert.AreEqual(n1.Versions.Count, 3);
+            n4.AddVersion(n5);
+            Assert.AreEqual(n1.Versions.Count, 4);
+            n2.AddVersion(n5);
+            Assert.AreEqual(n1.Versions.Count, 4);
+        }
+        [Test(Description = "IVersion test Add AddVersion")]
+        public void TestIVersionAddSelf()
+        {
+            TestNode n1 = new TestNode();
+            Assert.AreEqual(n1.Versions.Count, 0);
+            n1.AddVersion(n1);
+            Assert.AreEqual(n1.Versions.Count, 0);
+            TestNode n2 = new TestNode();
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n2.AddVersion(n2);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            n2.AddVersion(n1);
+            Assert.AreEqual(n1.Versions.Count, 1);
+            Assert.AreEqual(n2.Versions.Count, 0);
+        }
+        [Test(Description = "IVersion test OldVersion")]
+        public void TestIVersionOldOneVersion()
+        {
+            TestNode n1 = new TestNode();  
+            TestNode n2 = new TestNode();
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.OldVersions.Count, 0);
+            Assert.AreEqual(n2.OldVersions.Count, 1);
+        }
+        [Test(Description = "IVersion test OldVersion")]
+        public void TestIVersionOldTwoVersion()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            n1.AddVersion(n2);
+            n1.AddVersion(n3);
+            Assert.AreEqual(n1.OldVersions.Count, 0);
+            Assert.AreEqual(n2.OldVersions.Count, 1);
+            Assert.AreEqual(n3.OldVersions.Count, 2);
+        }
+        [Test(Description = "IVersion test OldVersion")]
+        public void TestIVersionOldManyVersion()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            TestNode n4 = new TestNode();
+            TestNode n5 = new TestNode();
+            TestNode n6 = new TestNode();
+            TestNode n7 = new TestNode();
+            n1.AddVersion(n2);
+            n1.AddVersion(n3);
+            n1.AddVersion(n4);
+            n3.AddVersion(n5);
+            n5.AddVersion(n6);
+            n2.AddVersion(n7);
+            Assert.AreEqual(n1.OldVersions.Count, 0);
+            Assert.AreEqual(n2.OldVersions.Count, 1);
+            Assert.AreEqual(n3.OldVersions.Count, 2);
+            Assert.AreEqual(n4.OldVersions.Count, 3);
+            Assert.AreEqual(n5.OldVersions.Count, 4);
+            Assert.AreEqual(n6.OldVersions.Count, 5);
+            Assert.AreEqual(n7.OldVersions.Count, 6);
+        }
+        [Test(Description = "IVersion test NextVersion")]
+        public void TestIVersionNextOneVersion()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();            
+            n1.AddVersion(n2);
+            Assert.AreEqual(n1.NextVersions.Count, 1);
+            Assert.AreEqual(n2.NextVersions.Count, 0);
+        }
+        [Test(Description = "IVersion test NextVersion")]
+        public void TestIVersionNextTwoVersion()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            n1.AddVersion(n2);
+            n1.AddVersion(n3);
+            Assert.AreEqual(n1.NextVersions.Count, 2);
+            Assert.AreEqual(n2.NextVersions.Count, 1);
+            Assert.AreEqual(n3.NextVersions.Count, 0);
+        }
+        [Test(Description = "IVersion test NextVersion")]
+        public void TestIVersionNextManyVersion()
+        {
+            TestNode n1 = new TestNode();
+            TestNode n2 = new TestNode();
+            TestNode n3 = new TestNode();
+            TestNode n4 = new TestNode();
+            TestNode n5 = new TestNode();
+            TestNode n6 = new TestNode();
+            TestNode n7 = new TestNode();
+            n1.AddVersion(n2);
+            n1.AddVersion(n3);
+            n1.AddVersion(n4);
+            n3.AddVersion(n5);
+            n5.AddVersion(n6);
+            n2.AddVersion(n7);
+            Assert.AreEqual(n1.NextVersions.Count, 6);
+            Assert.AreEqual(n2.NextVersions.Count, 5);
+            Assert.AreEqual(n3.NextVersions.Count, 4);
+            Assert.AreEqual(n4.NextVersions.Count, 3);
+            Assert.AreEqual(n5.NextVersions.Count, 2);
+            Assert.AreEqual(n6.NextVersions.Count, 1);
+            Assert.AreEqual(n7.NextVersions.Count, 0);           
+        }
     }
 }
--- MikTecnology/Conventions.cs
+++ MikTecnology/Conventions.cs
@@ -6,24 +6,89 @@ using System.Threading.Tasks;
 
 namespace MikTecnology
 {
+    //Convention for treeview style
     public interface INode
     {      
         void Add(INode node);
         void Remove(INode node);
     }
-    public abstract class BaseNode : INode
+    public interface IVersion
+    {
+        void AddVersion(IVersion node);
+        void SetBaseVersion(IVersion version);
+        void SetOldVersion(IList<IVersion> versions);
+        void SetNextVersion(IVersion version);
+    }
+    public abstract class BaseNode : INode,IVersion
     {
         private IList<INode> _nodes = new List<INode>();
-        public IList<INode> Nodes => _nodes;       
+        // List children nodes
+        public IList<INode> Nodes => _nodes;
+        private IList<IVersion> _versions = new List<IVersion>();
+        protected IList<IVersion> Versions => _versions;
+        //First version this parameter to be null
+        private IVersion _baseVersion;
+        private IList<IVersion> _oldVersion = new List<IVersion>();
+        private IList<IVersion> _nextVersion = new List<IVersion>();
+        public IList<IVersion> OldVersions => _oldVersion;
+        public IList<IVersion> NextVersions => _nextVersion;
         public virtual void Add(INode node)
         {
+            //Children don't have to self and repeated nodes
             if ((!this.Equals(node))&&(!_nodes.Contains(node)))
                 _nodes.Add(node);
         }
+
+        public void AddVersion(IVersion node)
+        {
+            //Children don't have to self and repeated versions
+            if ((!this.Equals(node))&& (!_versions.Contains(node)))
+            {
+                if (_baseVersion == null)
+                {
+                    //Write Next version for all old nodes (before added)
+                    foreach (IVersion version in this.Versions)
+                    {
+                        version.SetNextVersion(node);
+                    }
+                    //Add to first version
+                    _versions.Add(node);
+                    node.SetBaseVersion(this);
+                    node.SetOldVersion(this.Versions);
+                    this.SetNextVersion(node);//Write Next version for first  node
+                   
+                }
+                else
+                {
+                    //Add also to first version
+                    _baseVersion.AddVersion(node);
+                }               
+                
+            }
+                
+        }
+
         public void Remove(INode node)
         {
+            //Remove don't try to deleted no conteined nodes
             if (_nodes.Contains(node))
                 _nodes.Remove(node);
+        }
+
+        public void SetBaseVersion(IVersion version)//Next version always receive link to first version
+        {
+            _baseVersion = version;
+        }
+
+        public void SetOldVersion(IList<IVersion> versions)//Next version receive all versions before
+        {
+            foreach (IVersion version in versions)
+                _oldVersion.Add(version);
+        }
+
+        public void SetNextVersion(IVersion version)//Add next version
+        {
+            _nextVersion.Add(version);
         }
     }
 }
